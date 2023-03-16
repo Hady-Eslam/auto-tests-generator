d8fa47705a00da4a6d5c3e497ff27f15b7afa8b7
Hady Eslam <abdoaslam000@gmail.com>
2023-03-16 15:09:24 +0200

    Add Setup Package

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='auto_tests_generator',
+    version='0.1.0',
+    description='Auto Tests Generator',
+    url='https://github.com/Hady-Eslam/auto-tests-generator.git',
+    author='Hady Eslam',
+    author_email='abdoaslam000@gmail.com',
+    license='Apache',
+    packages=['auto_tests_generator'],
+    install_requires=[
+        'isort==4.3.21',
+        'black==23.1.0',
+        'autoflake==1.7.8',
+    ],
+
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
