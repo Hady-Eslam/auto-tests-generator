581b33473f4aaf020fea531a895b89e040e0e47a
Hady Eslam <abdoaslam000@gmail.com>
2023-05-22 11:45:11 +0300

    Add Signals Tests

---
+++ auto_tests_generator/signals/handler.py
@@ -0,0 +1,36 @@
+import sys
+import inspect
+from django.db.models import Model
+from auto_tests_generator.files import FilesHandler
+
+
+class SignalsHandler:
+
+    def __init__(self, files_handler: FilesHandler, apps) -> None:
+        self.__models = []
+        self.__files_handler = files_handler
+        self.__apps = apps
+
+    def load_models(self):
+        for name in list(sys.modules):
+            module = sys.modules[name]
+            if name.startswith(tuple(self.__apps)):
+                for member in inspect.getmembers(module, inspect.isclass):
+                    member[0]
+                    _member_class = member[1]
+                    if issubclass(_member_class, Model) and \
+                            _member_class.__module__ == name:
+                        self.__models.append(_member_class)
+
+    def generate_tests(self):
+        for model in self.__models:
+            self.__files_handler.generate_signals_tests_file(
+                model.__name__,
+                self.__files_handler.import_template(
+                    "signals/test_signals.txt", {
+                        "model": {
+                            "name": model.__name__,
+                        }
+                    }
+                )
+            )
